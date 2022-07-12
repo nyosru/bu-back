4f17323998e90fda649daf767790bc6f52f979b9
Сергей <1@php-cat.com>
2022-07-12 09:26:08 +0500

    666

---
+++ tests/Unit/CatalogHasTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class CatalogHasTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example2()
+    {
+        $this->assertTrue(true);
+    }
+}
